c1f48dc96da762b13cb7d2b0877e5131af1708dc
binRick <rick@blundell.com>
2023-06-14 19:42:49 -0400

    add products, genders testing cases

--- test.sh
+++ test.sh
@@ -6,7 +6,25 @@ if [[ ! -d .v ]]; then
 fi
 source .v/bin/activate
 BIRDS="$(find images/birds/ -type f|tr '\n' ',')"
+PRODUCTS="$(find images/products/ -type f|tr '\n' ',')"
+GENDERS="$(find images/genders/ -type f|tr '\n' ',')"
 
+products(){
+	cmd="time python3 ./test.py \
+		--model models/products.tflite \
+		--labels labels/products.csv \
+		--input $PRODUCTS"
+	echo -e "$cmd" >&2
+	eval "$cmd"
+}
+genders(){
+	cmd="time python3 ./test.py \
+		--model models/genders.tflite \
+		--labels labels/genders.txt \
+		--input $GENDERS"
+	echo -e "$cmd" >&2
+	eval "$cmd"
+}
 birds(){
 	cmd="time python3 ./test.py \
 		--model models/birds.tflite \
@@ -18,6 +36,8 @@ birds(){
 
 main(){
 	birds
+	products
+	#genders
 }
 
 main
